712cea0cbe7869ffb3469056a60f30f1d9cf7357
Weili Dai <weili.dai@turner.com>
2017-04-02 19:52:08 +0100

    Machine vs machine

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,6 +1,19 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { getRandomChoice } from '../game-logic';
 
 class Homepage extends PureComponent {
+
+    constructor(props, context) {
+        super(props, context);
+        this.onComputerVsComputerClick = this.onComputerVsComputerClick.bind(this)
+    }
+
+    onComputerVsComputerClick() {
+    	const randomChoice = getRandomChoice();
+        this.props.onChoiceClick(randomChoice);
+        this.props.history.push('/game-result');
+    }
 
 	render() {
 
@@ -8,10 +21,16 @@ class Homepage extends PureComponent {
 			<div className="homepage">
 				<h1 className="homepage__heading">Rock, Paper, Scissors</h1>
 				<div className="homepage__button" onClick={() => this.props.history.push('/rock-paper-scissors-choice')}>Human vs Computer</div>
-				<div className="homepage__button">Computer vs Computer</div>
+				<div className="homepage__button" onClick={this.onComputerVsComputerClick}>Computer vs Computer</div>
 			</div>
 		);
 	}
 }
 
-export default Homepage;
+function mapDispatchToProps(dispatch) {
+    return {
+        onChoiceClick: (type) => dispatch({type})
+    };
+}
+
+export default connect(null, mapDispatchToProps)(Homepage);
