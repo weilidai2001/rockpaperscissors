06f29803b92ebd2cc4a88aabe08cdc3823e6140c
Weili Dai <weili.dai@turner.com>
2017-04-03 07:48:47 +0100

    Refactored reducer to take arrays instead of switch statement

--- src/reducers/game-logic.js
+++ src/reducers/game-logic.js
@@ -1,15 +1,9 @@
 import { GameChoice } from '../constants/game-choices';
 
 export default function GameLogic(state = {}, action) {
-    switch (action.type) {
-        case GameChoice.CHOICE_ROCK:
-            return { choice: GameChoice.CHOICE_ROCK };
-        case GameChoice.CHOICE_PAPER:
-            return { choice: GameChoice.CHOICE_PAPER };
-        case GameChoice.CHOICE_SCISSORS:
-            return { choice: GameChoice.CHOICE_SCISSORS };
-
-        default:
-            return state;
-    }
+	if (Object.keys(GameChoice).includes(action.type)) {
+		return {choice: action.type};
+	} else {
+		return state;
+	}
 }
